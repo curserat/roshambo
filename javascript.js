d6542645566f6c5538883edfd632d95c3da4df6c
curserat <200~76402821+curserat@users.noreply.github.com>
2023-01-18 14:09:13 -0800

    Add getComputerChoice() for opponent option

---
+++ javascript.js
@@ -0,0 +1,23 @@
+
+//Get Computer's choice
+function getComputerChoice(){
+    const num = Math.floor(Math.random()*3)+1;
+    let choice;
+    switch (num){
+        case 1:
+            choice = 'Rock';
+            break;
+        case 2:
+            choice = "Paper";
+            break;
+        case 3:
+            choice = "Scissors";
+            break;
+        default:
+            alert("Something went wrong!");
+    }
+
+    return choice;
+}
+
+console.log(getComputerChoice());
